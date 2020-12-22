84860c0fba998c4dac008c4a045089af66d0b32f
muyildirim <musyil@netrd.com.tr>
2020-12-22 10:14:19 +0300

    parent prop change is added

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,25 +1,41 @@
-import { useReactMediaRecorder } from "react-media-recorder";
-import React, { useState, useRef } from "react";
+import {useReactMediaRecorder} from "react-media-recorder";
+import React, {useEffect, useState} from "react";
 
-const Question = () => {
-    const [questionMedia, setQuestionMedia] = useState('');
+const Question = (props) => {
+    const [parentStatus, setParentStatus] = useState('');
     const {
         status,
         startRecording,
         stopRecording,
         mediaBlobUrl,
+        clearBlobUrl
     } = useReactMediaRecorder({
         video: false,
         audio: true,
         type: "audio/wav"
     });
 
+    useEffect(() => {
+        if (mediaBlobUrl) {
+            props.parentCallBack(mediaBlobUrl)
+        }
+
+    }, [mediaBlobUrl]);
+
+    useEffect(() => {
+        if (props.isReset) {
+            console.log("passed reset: " + props.isReset)
+            clearBlobUrl()
+            console.log("mediaUrl: " + mediaBlobUrl)
+            props.setReset(false)
+        }
+    }, [props.isReset]);
     return (
         <div>
             <p>{status}</p>
             <button onClick={startRecording}>Start Recording</button>
             <button onClick={stopRecording}>Stop Recording</button>
-            <video src={mediaBlobUrl} controls autoplay loop />
+            <video src={mediaBlobUrl} controls autoplay loop/>
         </div>
     );
 };
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,36 +1,28 @@
-import { useReactMediaRecorder } from "react-media-recorder";
-import React, {useState, useRef, useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Question from "./Question";
-import AuthService from "../services/auth.service";
-import {Link} from "react-router-dom";
+
 const QuestionForm = () => {
 
 
     const [questionMedia, setQuestionMedia] = useState('');
-    const [answerMedia, setAnswerMedia] = useState(null);
-    const [isNextQuestion, setIsNextQuestion] = useState(false);
-    const [questionNumber, setQuestionNumber] = useState(1);
-    const [currentQuestion, setCurrentQuestion] = useState(<Question></Question>);
+    const [reset, setReset] = useState(false);
     useEffect(() => {
-        console.log("props: ")
-        console.log(JSON.stringify())
-    }, [currentQuestion]);
+
+    }, [reset]);
 
 
     const nextQuestion = () => {
-        if(!isNextQuestion ){
-            alert('Process is not completed! check your inputs')
-        }
-        else{
-            // current question will be send to back end service
-            console.log("Question is sent to back end");
-
-        }
+        setReset(true)
+        handleMediaBlob(null)
+    };
+    const handleMediaBlob = (questionMedia) => {
+        setQuestionMedia(questionMedia);
+        console.log("Parent media " + questionMedia)
     };
     return (
         <div>
             <button onClick={nextQuestion}> Move to Next Question</button>
-            {currentQuestion}
+            <Question parentCallBack={handleMediaBlob} isReset={reset} setReset={setReset}></Question>
             <button> Finish Interview Form</button>
         </div>
     );
